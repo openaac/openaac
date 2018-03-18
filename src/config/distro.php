3e2f9e577a2c0853b8357e1899939eb797502ace
Christopher Eklund <eklundchristopher@gmail.com>
2018-03-18 04:21:16 +0100

    Initial commit

---
+++ src/config/distro.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Distribution Provider
+    |--------------------------------------------------------------------------
+    |
+    | Specify the distribution provider class of the distribution package that
+    | you would like to utilise. A distribution provider helps pandaac with
+    | recognising the differences between the available distributions.
+    |
+    | Default: pandaac\Distributions\TFS12\Providers\DistributionProvider::class
+    |
+    */
+
+    'provider' => pandaac\Distributions\TFS12\Providers\DistributionProvider::class,
+
+];
